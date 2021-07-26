f826d905b7f674b5a47acab75969c3d6f111b177
Icrani <Icrani335012@163.com>
2021-07-26 17:36:45 +0800

    update

--- src/store/home.js
+++ src/store/home.js
@@ -1,16 +1,31 @@
+import {reqCategoryList} from "@/api";
 //这个是home的vuex的模块
-
 const state = {
     //存数据
+    categoryList: []
 }
 
-const mutations={
+const mutations = {
 //直接修改数据
+    RECEIVE_CATEGORYLIST(state, categoryList) {
+        state.categoryList = categoryList
+    }
 }
 const actions = {
     //与组件当中与用户对接
     //一般是异步发请求
     //提交mutations
+    async getCategoryList({commit}) {  //这里是直接使用解构赋值
+        //发送请求拿数据，提交给mutations，存储到state
+        //await和async作用
+        /*可以通过同步代码实现异步效果，可读性强*/
+        //.then().catch()不是说不能用，可读性不强，里面还是有异步代码(异步回调函数)
+        const result = await reqCategoryList()
+        if (result.code === 200) {
+            commit('RECEIVE_CATEGORYLIST',result.data)
+        }
+
+    }
 }
 const getters = {
     //后面用来简化数据的操作
