8686f03984dc16cc38b8554754a18e96ca65802b
Icrani <Icrani335012@163.com>
2021-08-23 09:09:04 +0800

    update

--- mock/index.js
+++ mock/index.js
@@ -6,18 +6,20 @@ const JSON5 = require('json5')
 //读取json文件
 function getJsonFile(filePath) {
     //读取指定json文件
-    var json = fs.readFileSync(path.resolve(__dirname,filePath),"utf-8")
+    var json = fs.readFileSync(path.resolve(__dirname, filePath), "utf-8")
     //解析并返回
     return JSON5.parse(json);
 }
 
 //返回一个函数
 module.exports = function (app) {
-    //监听http请求
-    app.get('/user/userinfo',function (rep, res) {
-        //每次响应请求时读取mock data的json文件
-        //getJsonFile方法定义了如何读取json文件并解析成数据对象
-        var json = getJsonFile('./userInfo.json5')
-        res.json(Mock.mock(json))
-    })
+    if (process.env.MOCK == 'true') {
+        //监听http请求
+        app.get('/user/userinfo',function (rep, res) {
+            //每次响应请求时读取mock data的json文件
+            //getJsonFile方法定义了如何读取json文件并解析成数据对象
+            var json = getJsonFile('./userInfo.json5')
+            res.json(Mock.mock(json))
+        })
+    }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
+    <div>
+      123132
+    </div>
     <Header/>
     <router-view></router-view>
-<!--    可以这么写，但是如果后期很多个页面不需要，则太过于麻烦-->
-<!--    <Footer v-show="$route.path !== '/login' && $route.path !== '/register'"/>-->
+    <!--    可以这么写，但是如果后期很多个页面不需要，则太过于麻烦-->
+    <!--    <Footer v-show="$route.path !== '/login' && $route.path !== '/register'"/>-->
     <Footer v-show="!$route.meta.isHidden"></Footer>
 
   </div>
@@ -12,11 +15,28 @@
 <script>
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import axios from "axios";
+
 export default {
   name: 'App',
-  components:{
+  components: {
     Header,
     Footer
+  },
+  mounted() {
+    //这里是测试mock
+    const baseurl = 'https://www.fastmock.site/mock/dd94c6b113866e273d0b3b63158664a2/api'
+
+    axios.get(`${baseurl}/test`)
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    //这里是测试mock
+
+
   }
 }
 </script>
